c6d9d69e9764a310908e278ba22dcdbf5938eb7d
Fabrice K.M. Ekpetse <fabrice.xyclone@gmail.com>
2022-03-05 20:28:54 +0000

    Added `DTF` library

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const MDP = require('./lib/MDP')
-const APS = require('./lib/APS')
-const WPS = require('./lib/WPS')
-const BND = require('./lib/BND')
-const CSA = require('./lib/CSA')
+const MDP = require('./lib/MDP') // Main Data Provider
+const APS = require('./lib/APS') // Authentication Process Server
+const WPS = require('./lib/WPS') // Webhook Protocol Server
+const BND = require('./lib/BND') // Bulk Notification Dispatcher
+const CSA = require('./lib/CSA') // Cubic Server API
+const DTF = require('./lib/DTF') // Delta to File
 
-module.exports = { MDP, APS, WPS, BND, CSA }
+module.exports = { MDP, APS, WPS, BND, CSA, DTF }
